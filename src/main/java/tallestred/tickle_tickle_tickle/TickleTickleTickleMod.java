1508249f0b733e59959f4c06d7ab0b21204ec372
seymourimadeit <saulgoodman53434344@gmail.com>
2025-04-08 11:24:38 -0700

    tickle only once

--- src/main/java/tallestred/tickle_tickle_tickle/TickleTickleTickleMod.java
+++ src/main/java/tallestred/tickle_tickle_tickle/TickleTickleTickleMod.java
@@ -1,41 +1,30 @@
 package tallestred.tickle_tickle_tickle;
 
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.damagesource.DamageSources;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.monster.Enemy;
-import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.projectile.ProjectileUtil;
-import net.minecraft.world.item.BowItem;
-import net.minecraft.world.item.BrushItem;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.GameRules;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.util.ObfuscationReflectionHelper;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.entity.living.LivingChangeTargetEvent;
-import net.neoforged.neoforge.event.entity.living.LivingEvent;
-import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
-import net.neoforged.neoforge.event.tick.EntityTickEvent;
-import net.neoforged.neoforge.event.tick.PlayerTickEvent;
-import tallestred.tickle_tickle_tickle.common.data_attachments.TTTDataAttachments;
-import tallestred.tickle_tickle_tickle.config.Config;
-
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.util.ObfuscationReflectionHelper;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.entity.living.LivingChangeTargetEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import tallestred.tickle_tickle_tickle.common.data_attachments.TTTDataAttachments;
+import tallestred.tickle_tickle_tickle.config.Config;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
+
 @Mod(TickleTickleTickleMod.MODID)
 public class TickleTickleTickleMod {
     public static final String MODID = "tickle_tickle_tickle";
@@ -53,7 +42,7 @@ public class TickleTickleTickleMod {
         Player player = event.getEntity();
         ItemStack stack = event.getItemStack();
         LivingEntity hostile = (LivingEntity) event.getTarget();
-        if (stack.canPerformAction(net.neoforged.neoforge.common.ItemAbilities.BRUSH_BRUSH) && hostile instanceof Enemy) {
+        if (stack.canPerformAction(net.neoforged.neoforge.common.ItemAbilities.BRUSH_BRUSH) && hostile instanceof Enemy && hostile.getData(TTTDataAttachments.TICKLED.get())) {
             player.swing(event.getHand(), true);
             if (!player.level().isClientSide) {
                 dropLoot.invoke(hostile, hostile.damageSources().generic(), false);
@@ -63,7 +52,10 @@ public class TickleTickleTickleMod {
                 hostile.spawnAtLocation(itemstack);
                 hostile.setItemSlot(equipmentslot, ItemStack.EMPTY);
             }
-            ((Mob)hostile).setTarget(null);
+            if (hostile instanceof Mob) {
+                ((Mob) hostile).setTarget(null);
+                ((Mob) hostile).setAggressive(false);
+            }
             hostile.setData(TTTDataAttachments.TICKLED.get(), true);
             event.setCancellationResult(InteractionResult.SUCCESS);
         }
